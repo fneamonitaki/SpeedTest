5d73a31ed669eb958089f8cea08b08e886febe85
fneamonitaki <zneamonitaki@isc.tuc.gr>
2025-05-20 18:54:12 +0300

    add comments

--- client.c
+++ client.c
@@ -7,74 +7,85 @@
 #include <time.h>
 #include <sys/time.h>
 
-//#define SERVER_IP "147.27.116.242" // <-- CHANGE to your server's IP
+
 #define SERVER_IP "127.0.01.1" //local IP for demo
-//#define SERVER_IP "192.168.1.5" //local IP for demo
+//#define SERVER_IP "192.168.1.5" //IP for testings
 #define SERVER_PORT 5555
-#define BUFFER_SIZE (16384) // o client stelnei ara to write
-#define TEST_DURATION 32  // seconds
-#define INTERVAL 2        // seconds
+#define BUFFER_SIZE (64 * 1024) //This is the write buffer size because the client is sending data
+#define TEST_DURATION 32  
+#define INTERVAL 2        
 #define SA struct sockaddr
 
-// Convert bytes to Mbps
+
+/**
+ * Function to convert bytes to mbps
+ * (based on wikipedia)
+ * 1 Mbps = 1,000,000 bps
+ */
 double bytes_to_mbps(size_t bytes, double seconds) {
-    return (bytes * 8.0) / (seconds * 1000000.0);  // 1 Mbps = 1,000,000 bps
+    return (bytes * 8.0) / (seconds * 1000000.0);  
 }
 
 int main() {
     int sock;
     struct sockaddr_in serv_addr;
+    //alocate a buffer for data
     char buffer[BUFFER_SIZE];
-    memset(buffer, 'A', BUFFER_SIZE);  // Fill buffer with dummy data
+    //this fills the buffer with dummy data
+    memset(buffer, 'A', BUFFER_SIZE);  
 
-    // Create socket
+    //create socket with the same way as server
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Socket creation failed");
+        printf("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    int size = 212992; // ή 524288 για μεγαλύτερο
-    setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &size, sizeof(size));
-    
-    // Setup server address
+    //assign IP, PORT
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(SERVER_PORT);
-    serv_addr.sin_addr.s_addr = inet_addr(SERVER_IP);  // Use real IP
+    serv_addr.sin_addr.s_addr = inet_addr(SERVER_IP); 
 
-    // Connect to server
+    //connect the client socket to server socket
     if (connect(sock, (SA*)&serv_addr, sizeof(serv_addr)) != 0) {
-        perror("Connection failed");
+        printf("Connection failed");
         close(sock);
         exit(EXIT_FAILURE);
     }
     printf("Connected to server.\n");
 
-    // Timing setup
+    //start 2 timers, one for the 20 sec duartion and one for 2 sec interval
     struct timeval start_time, current_time, interval_start;
     gettimeofday(&start_time, NULL);
     gettimeofday(&interval_start, NULL);
 
+    //initialize counters
     size_t total_sent = 0;
     size_t sent_this_interval = 0;
 
     while (1) {
-        // Send data
+        //sending data
         ssize_t sent = send(sock, buffer, BUFFER_SIZE, 0);
-        if (sent < 0) {
-            perror("Send error");
+        if (sent < 0) { //if something goes wrong
+            printf("Send error");
             break;
         }
+
+        //update counters
         total_sent += sent;
         sent_this_interval += sent;
 
-        // Check time
+        //start a current timer 
         gettimeofday(&current_time, NULL);
+
+        //The following code is for calculate timing
+        //->measuring elapsed time with microsecond precision
         double elapsed_total = (current_time.tv_sec - start_time.tv_sec) +
                                (current_time.tv_usec - start_time.tv_usec) / 1000000.0;
 
         double elapsed_interval = (current_time.tv_sec - interval_start.tv_sec) +
                                   (current_time.tv_usec - interval_start.tv_usec) / 1000000.0;
 
+        //every 2 secs print throuput 
         if (elapsed_interval >= INTERVAL) {
             double mbps = bytes_to_mbps(sent_this_interval, elapsed_interval);
             static int interval_number = 0;
@@ -83,18 +94,20 @@ int main() {
                 (interval_number + 1) * INTERVAL,
                 mbps);
             interval_number++;
+            //reset the interval timer and counter
             sent_this_interval = 0;
             gettimeofday(&interval_start, NULL);
 }
-
+        //this is ending the loop after 30 secs
         if (elapsed_total >= TEST_DURATION) {
             break;
         }
     }
-
+    //at the end we calculate the total average throughput and print it
     double final_mbps = bytes_to_mbps(total_sent, TEST_DURATION);
     printf("[CLIENT] Finished. Total sent: %zu bytes (%.2f Mbps avg)\n", total_sent, final_mbps);
 
+    //close the socket
     close(sock);
     return 0;
 }
--- server.c
+++ server.c
@@ -7,39 +7,66 @@
 #include <sys/time.h>
 
 #define PORT 5555
-#define BUFFER_SIZE (131072) //o server lambanei ara to read
+#define BUFFER_SIZE (64 * 1024) //This is the read buffer size because the server is receiving data
 #define TEST_DURATION 34
 #define INTERVAL 2
 #define SA struct sockaddr
 
+/**
+ * Function to convert bytes to mbps
+ * (based on wikipedia)
+ */
 double bytes_to_mbps(size_t bytes, double seconds) {
     return (bytes * 8.0) / (seconds * 1000000.0);
 }
 
+/**
+ * function to handle each client at once for 30sec
+ * ->receives TCP data from the client for 30 seconds
+ * ->prints throughput in 2-second intervals
+ * and 
+ * ->prints average throughput at the end
+ */
 void handle_client(int client_fd) {
+    //alocate a buffer for data
     char buffer[BUFFER_SIZE];
     struct timeval start_time, current_time, interval_start;
+    //starting two timers
+    //start_time is for the 30 seconds
+    //interval_start is for 2sec windows
     gettimeofday(&start_time, NULL);
     gettimeofday(&interval_start, NULL);
 
+    //initializing counter for total and interval data
     size_t total_received = 0;
     size_t received_this_interval = 0;
 
     while (1) {
+        //read data from client
         ssize_t received = recv(client_fd, buffer, BUFFER_SIZE, 0);
-        if (received <= 0)
+        //if this is true then something went wrong with the client
+        //so exit the while loop
+        if (received <= 0) 
             break;
 
+        //update counters
         total_received += received;
         received_this_interval += received;
 
+
+
+        //start a current timer 
         gettimeofday(&current_time, NULL);
+
+        //The following code is for calculate timing
+        //->measuring elapsed time with microsecond precision
         double elapsed_total = (current_time.tv_sec - start_time.tv_sec) +
                                (current_time.tv_usec - start_time.tv_usec) / 1000000.0;
 
         double elapsed_interval = (current_time.tv_sec - interval_start.tv_sec) +
                                   (current_time.tv_usec - interval_start.tv_usec) / 1000000.0;
 
+        //every 2 secs print throuput                           
         if (elapsed_interval >= INTERVAL) {
             double mbps = bytes_to_mbps(received_this_interval, elapsed_interval);
             static int interval_number = 0;
@@ -48,16 +75,20 @@ void handle_client(int client_fd) {
                 (interval_number + 1) * INTERVAL,
                 mbps);
             interval_number++;
+            //reset the interval timer and counter
             received_this_interval = 0;
             gettimeofday(&interval_start, NULL);
 }
 
+        //this is ending the loop after 30 secs
         if (elapsed_total > TEST_DURATION)
             break;
     }
 
+    //at the end we calculate the total average throughput and print it
     double avg_mbps = bytes_to_mbps(total_received, TEST_DURATION);
     printf("[SERVER] Finished. Total received: %zu bytes (%.2f Mbps avg)\n", total_received, avg_mbps);
+    //clode socket of client
     close(client_fd);
 }
 
@@ -66,41 +97,59 @@ int main() {
     struct sockaddr_in servaddr, cliaddr;
     socklen_t len = sizeof(cliaddr);
 
+    /***
+     * PF_INET = Internet version 4 protocols == AF_INET 
+     * SOCK_STREAM = the socket has the indicated type, which specifies the semantics of communication
+     * SOCK_STREAM = we use it for TCP! 
+     * don't need to set protocol unless raw sockets! (thats why the 3rd attribute is 0)
+     */
+
+     //create socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
-        perror("Socket creation failed");
         printf("Socket creation failed\n\n");
         exit(EXIT_FAILURE);
     }
     
-
+    //bind socket, we need to bind an address to a local port
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET; //IPv4
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);  // Accept from any IP
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); //this is for accepting from all IP's
     servaddr.sin_port = htons(PORT);
 
     if (bind(server_fd, (SA*)&servaddr, sizeof(servaddr)) < 0) {
-        perror("Bind failed");
+        printf("Bind failed\n");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
+    /**
+     * we need to specify our socket and 5 is to specify a backlog,
+     * which is how many outstanding connections the kernel can be waiting to pass off to
+     * our aplication, if our application is slow to deal with inbound connections
+     */
+
+    //listen
     if (listen(server_fd, 5) != 0) {
-        perror("Listen failed");
+        printf("Listen failed!");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
     printf("Server listening on port %d...\n", PORT);
-
+    
+    // Accept client connection
+    //the server is running all time
     while (1) {
+        //when we execute accept the system call will block until we get a new connection
         client_fd = accept(server_fd, (SA*)&cliaddr, &len);
         if (client_fd < 0) {
-            perror("Accept failed");
+            printf("Accept failed");
             continue;
         }
 
         printf("Client connected from %s\n", inet_ntoa(cliaddr.sin_addr));
+        //then the handle client is called and handles the 2 sec interval 
         handle_client(client_fd);
         printf("Ready for next client...\n\n");
     }
